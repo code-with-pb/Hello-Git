c447cee3c6a7256abcb96770da66c23390ce4a51
code-with-pb <codexpythonpb@gmail.com>
2023-04-21 01:19:57 +0530

    for

---
+++ P2.java
@@ -0,0 +1,10 @@
+class P2{
+    public static void main(String args[]){
+        int a;
+        for (int i =0;i<5;i++){
+            a = 0;
+            a+=i;
+            System.out.println(a);
+        }
+    }
+}
